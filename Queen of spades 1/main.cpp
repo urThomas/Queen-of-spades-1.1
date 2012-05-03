4df871f05bd3a5749f0077c067277a7f0ddf0cfd
Thomas Ur <thomasur@dhcp-047169.wlan.ntnu.no>
2012-05-03 15:02:20 +0200

    Initial Commit

---
+++ Queen of spades 1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Queen of spades 1
+//
+//  Created by Thomas Ur on 03.05.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
